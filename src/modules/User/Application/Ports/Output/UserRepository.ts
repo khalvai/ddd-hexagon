f51ab65ecff52d35cf4d85055b13ff0eaad95914
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-03 20:00:39 +0330

    feat: loadByEmail method is added to user repository.

--- src/modules/User/Application/Ports/Output/UserRepository.ts
+++ src/modules/User/Application/Ports/Output/UserRepository.ts
@@ -1,4 +1,6 @@
+import { Inject } from "@nestjs/common";
 import Result from "src/modules/Common/Application/Result";
+import Email from "src/modules/User/Domain/Email";
 import User from "src/modules/User/Domain/User";
 import UserId from "src/modules/User/Domain/UserId";
 
@@ -6,6 +8,7 @@ export const UserRepository = Symbol("UserRepository").valueOf();
 export interface UserRepository
 {
     load(userId: UserId): Promise<Result<User>>;
+    loadByEmail(email: Email): Promise<Result<User>>;
     save(user: User): Promise<Result<void>>;
 
 }

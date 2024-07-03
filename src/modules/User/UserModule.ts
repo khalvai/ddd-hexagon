d8d9e3e3b5e31a3ebd10d764dd97a84cd28fcdbe
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-03 20:01:39 +0330

    feat: imported user, outbox mapper.

--- src/modules/User/UserModule.ts
+++ src/modules/User/UserModule.ts
@@ -1,5 +1,6 @@
-import { AutomapperModule } from "@automapper/nestjs";
 import { Module } from "@nestjs/common";
+import { CqrsModule } from "@nestjs/cqrs";
+import { Prisma } from "@prisma/client";
 import { EncryptionService } from "src/modules/Common/Application/Output/EncryptionService";
 import { HashService } from "src/modules/Common/Application/Output/HashService";
 import { KeyStore } from "src/modules/Common/Application/Output/KeyStore";
@@ -11,27 +12,38 @@ import JWKeyStore from "src/modules/Common/Infrastructure/Adapters/Output/JWKeyS
 import JWSigningService from "src/modules/Common/Infrastructure/Adapters/Output/JWSigningService";
 import JWTokenService from "src/modules/Common/Infrastructure/Adapters/Output/JWTokenService";
 import { RegisterUseCaseImpl } from "src/modules/User/Application/Ports/Input/RegisterUseCaseImpl";
+import { EmailServiceProvider } from "src/modules/User/Application/Ports/Output/EmailServiceProvider";
+import { OutboxRepository } from "src/modules/User/Application/Ports/Output/OutboxRepository";
+import { Template } from "src/modules/User/Application/Ports/Output/Template";
 import { UserRepository } from "src/modules/User/Application/Ports/Output/UserRepository";
-import { RegisterUseCase } from "src/modules/User/Application/UseCases/Register";
 import UserHTTPPInputAdapter from "src/modules/User/Infrastructure/Adapters/Input/UserHTTPInputAdapter";
-
+import { LiaraEmailServiceProvider } from "src/modules/User/Infrastructure/Adapters/Output/LiaraEmailServiceProvider";
+import { OutboxMapper } from "src/modules/User/Infrastructure/Adapters/Output/Mapper/OutboxMapper";
+import UserMapper from "src/modules/User/Infrastructure/Adapters/Output/Mapper/UserMapper";
+import PostgresqlOutboxRepository from "src/modules/User/Infrastructure/Adapters/Output/Persistence/PostgresqlOutboxRepository";
 import { PostgresqlUserRepository } from "src/modules/User/Infrastructure/Adapters/Output/Persistence/PostgresqlUserRepository";
 import PrismaModule from "src/modules/User/Infrastructure/Adapters/Output/Persistence/PrismaModul";
+import { EJSTemplate } from "src/modules/User/Infrastructure/Adapters/Output/Template/EjsTemplate";
 
 
 @Module({
     imports: [
-        PrismaModule, ],
+        PrismaModule,
+        CqrsModule,
+    ],
     controllers: [ UserHTTPPInputAdapter ],
     providers: [
-
         {
             provide: UserRepository,
-            useClass: PostgresqlUserRepository
+            useClass: PostgresqlUserRepository,
         },
         {
-            provide: RegisterUseCase,
-            useClass: RegisterUseCaseImpl
+            provide: PostgresqlOutboxRepository,
+            useClass: PostgresqlOutboxRepository
+        },
+        {
+            provide: OutboxRepository,
+            useClass: PostgresqlOutboxRepository
         },
         {
             provide: TokenService,
@@ -53,8 +65,18 @@ import PrismaModule from "src/modules/User/Infrastructure/Adapters/Output/Persis
         {
             provide: SigningService,
             useClass: JWSigningService
-        }
-
+        },
+        {
+            provide: Template,
+            useClass: EJSTemplate
+        },
+        {
+            provide: EmailServiceProvider,
+            useClass: LiaraEmailServiceProvider
+        },
+        RegisterUseCaseImpl,
+        OutboxMapper,
+        UserMapper,
     ]
 })
 export class UserModule { }

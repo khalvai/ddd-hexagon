64f118438caa7baa92aea98828bc5a658b5da580
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-03 19:53:51 +0330

    feat: http exception filter is added to catch errors that occur during runtime.

---
+++ src/modules/Common/Infrastructure/Adapters/Output/HttpExceptionFilter.ts
@@ -0,0 +1,74 @@
+import
+{
+    ArgumentsHost,
+    Catch,
+    ExceptionFilter,
+    HttpException,
+    HttpStatus,
+    InternalServerErrorException
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost } from '@nestjs/core';
+import { NotValidInput } from 'src/modules/User/Domain/Exceptions/NotValidInput';
+// import * as Sentry from '@sentry/node';
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter
+{
+    constructor (
+        private readonly httpAdapterHost: HttpAdapterHost,
+    ) { }
+
+    catch(exception: HttpException, host: ArgumentsHost): any
+    {
+        const { httpAdapter } = this.httpAdapterHost;
+
+        const ctx = host.switchToHttp();
+        let message: any = "INTERNAL_SERVER_ERROR";
+        const httpStatus =
+            exception instanceof HttpException
+                ? exception.getStatus()
+                : HttpStatus.INTERNAL_SERVER_ERROR;
+
+
+
+        const responseBody = {
+            statusCode: httpStatus,
+            timestamp: new Date().toISOString(),
+            path: httpAdapter.getRequestUrl(ctx.getRequest()),
+            message
+        };
+
+
+
+        if (exception instanceof HttpException && exception.getStatus() !== 500)
+        {
+            responseBody.message = exception.getResponse();
+        }
+
+
+
+
+        if (exception instanceof NotValidInput)
+        {
+            responseBody.message = exception.cause;
+        }
+
+
+
+
+        httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+
+        if (httpStatus === HttpStatus.INTERNAL_SERVER_ERROR)
+        {
+
+
+            const requestDetail = {
+                path: httpAdapter.getRequestUrl(ctx.getRequest()),
+                method: httpAdapter.getRequestMethod(ctx.getRequest()),
+                error: exception
+            };
+            console.log(requestDetail);
+
+        }
+    }
+}

edcf6535707f468d73070dbabe116ec3810764ec
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-03 19:54:34 +0330

    feat: ejs template class implements template interface.

---
+++ src/modules/User/Infrastructure/Adapters/Output/LiaraEmailServiceProvider.ts
@@ -0,0 +1,15 @@
+import Result from "src/modules/Common/Application/Result";
+import { EmailServiceProvider } from "src/modules/User/Application/Ports/Output/EmailServiceProvider";
+import Email from "src/modules/User/Domain/Email";
+
+
+export class LiaraEmailServiceProvider implements EmailServiceProvider
+{
+    async send(template: string, to: Email): Promise<Result<void>>
+    {
+
+        // talk to a 3rd party to send email
+
+        return Result.ok();
+    }
+}

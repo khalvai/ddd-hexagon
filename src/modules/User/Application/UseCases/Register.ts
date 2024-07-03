f26f24edac44914a4839f241e0cc628a856875f0
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-03 20:00:07 +0330

    fix: used command bus config to use command bus for register command.

--- src/modules/User/Application/UseCases/Register.ts
+++ src/modules/User/Application/UseCases/Register.ts
@@ -1,5 +1,6 @@
+import { ICommandHandler } from "@nestjs/cqrs";
 import { CommandHandler } from "src/modules/Common/Application/CommandHandler";
+import Result from "src/modules/Common/Application/Result";
 import { RegisterCommand } from "src/modules/User/Application/Commands/RegisterCommand";
 
-export const RegisterUseCase = Symbol("RegisterUseCase").valueOf();
-export interface RegisterUseCase extends CommandHandler<RegisterCommand> { }
+export interface RegisterUseCase extends ICommandHandler<RegisterCommand, Result<void>> { }

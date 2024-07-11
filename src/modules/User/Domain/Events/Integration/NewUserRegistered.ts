71273f821c763840589e78a865185d93c9f6f6c5
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-12 00:25:57 +0330

    refactor: new user registered event is changed.

--- src/modules/User/Domain/Events/Integration/NewUserRegistered.ts
+++ src/modules/User/Domain/Events/Integration/NewUserRegistered.ts
@@ -1,5 +1,6 @@
 import DomainEvent from "src/modules/Common/Domain/DomainEvent";
 import IP from "src/modules/User/Domain/IP";
+import User from "../../User";
 
 interface EventPayload {
     userId: string;
@@ -8,7 +9,12 @@ interface EventPayload {
     ip: string;
 }
 export default class NewUserRegistered extends DomainEvent<EventPayload, "Integration"> {
-    public constructor(userId: string, email: string, name: string, ip: string) {
+    private constructor(userId: string, email: string, name: string, ip: string) {
         super({ email, name, userId, ip });
     }
+
+    static of(user: User, ip: IP): NewUserRegistered {
+        return new NewUserRegistered(user.userId.value, user.email.value, user.name.value, ip.value)
+    }
+
 }
